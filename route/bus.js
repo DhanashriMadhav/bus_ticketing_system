cc1ea6f08ff6f62a44790073ae47e5b6e0520660
shubhnagi <shubhangis20@navgurukul.org>
2021-04-14 19:29:42 +0530

    API

--- route/bus.js
+++ route/bus.js
@@ -88,30 +88,28 @@ router.post('/tickets',auth,async(req,res)=>{
 
 })
 
-//reset the ticket using ticketId
-router.put('/reset/:busId/:_id',auth,async(req,res)=>{
+//reset the ticket using busId
+router.put('/reset/:busId',auth,async(req,res)=>{
     try
     {
         const user=await User.findById(req.user.id)
         const isAdmin=user.isAdmin
         if(isAdmin===true)
         {
-            const _id=req.params._id
-            // console.log(_id)
+            
             const busId=req.params.busId
             let busid= await Ticket.find({busId})
             if(busid.length===0)
             {
                 return res.status(404).json({msg:"Tickets not found for this bus"})
             }
-            const ticket =await Ticket.find({busId,_id})
-            const isBooked=ticket[0].isBooked
-            const userId=ticket[0].userId
-            if(isBooked===true && userId)
+            const ticket =await Ticket.find({busId,isBooked:true})
+            console.log(ticket)
+            if(ticket.length!==0)
             {
-                await Ticket.updateOne({busId,_id},{$unset:{userId:""}})
-                await Ticket.updateOne({busId,_id},{$set:{isBooked:false}})
-                return res.status(200).json({msg:"ticket is open "})
+                await Ticket.updateMany({busId},{$unset:{userId:""}})
+                await Ticket.updateMany({busId},{$set:{isBooked:false}})
+                return res.status(200).json({msg:"Ticket is open to book"})
             }
             else
             {
